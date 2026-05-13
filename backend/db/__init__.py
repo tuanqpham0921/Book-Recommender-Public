39c1c207343621dc5717005e6810b36c89172c20
Tuan Pham <tuanqpham0921@gmail.com>
2026-05-13 13:48:19 -0500

    remove book store from __init__

--- backend/db/__init__.py
+++ backend/db/__init__.py
@@ -1,6 +1,5 @@
 from .async_engine import get_async_engine, get_session_factory, close_async_engine, check_connection
 from .bootstrap import bootstrap_schema, create_indexes, enable_extensions, init_tables
-from .stores.book_store import BookStore
 from .readiness import (
     CheckResult,
     ReadinessReport,
@@ -17,7 +16,6 @@ __all__ = [
     "enable_extensions",
     "init_tables",
     "create_indexes",
-    "BookStore",
     "CheckResult",
     "ReadinessReport",
     "is_ready",
